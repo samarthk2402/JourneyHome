511859a5d74a1bac126db88ddf174f9feb5ae163
sammydoodles <samarthk2402@gmail.com>
2023-03-15 16:52:31 +0000

    Spawned a puddle when slime falls

--- Assets/Scripts/DamageOnCollision.cs
+++ Assets/Scripts/DamageOnCollision.cs
@@ -4,14 +4,25 @@ using UnityEngine;
 
 public class DamageOnCollision : MonoBehaviour
 {
-    public float damage = 20f;
+    public float damage = 100f;
+    public int hit_num;
+    public bool isDot;
+    public GameObject puddle;
     void OnCollisionEnter(Collision collision){
         if(collision.gameObject.layer == 6)
         {
-            collision.gameObject.GetComponent<PlayerTarget>().TakeDamage(damage);
+            if(isDot){
+                collision.gameObject.GetComponent<PlayerTarget>().TakeDamageOverTime(damage, 10);
+                Debug.Log("Hit");
+            }else{
+                collision.gameObject.GetComponent<PlayerTarget>().TakeDamage(damage);
+                Destroy(this.gameObject);
+            }   
+        }else if(collision.gameObject.layer == 3 && !isDot){
+            ContactPoint cp = collision.GetContact(0);
+            Instantiate(puddle, cp.point+new Vector3(0, 0, 0.3f), Quaternion.FromToRotation (Vector3.up, cp.normal));
             Destroy(this.gameObject);
-        }else if(collision.gameObject.layer == 3){
-            Destroy(this.gameObject);
+
         }
     }
     
